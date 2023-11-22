2c7638837896280806ca491155248eab3c0fbc62
jahir-biz <jahir.uddin@bizcloudexperts.com>
2023-11-23 01:06:09 +0530

    log insertion code updated

--- src/ltl_rating_log_insertion/index.js
+++ src/ltl_rating_log_insertion/index.js
@@ -1,3 +1,50 @@
-module.exports.handler = (event, context) => {
+const { get } = require("lodash");
+const { v4 } = require("uuid");
+const { LTL_LOG_TABLE } = process.env;
+const moment = require("moment-timezone");
+let functionName;
+module.exports.handler = async (event, context) => {
     console.info(`🙂 -> file: index.js:2 -> event:`, event);
+    functionName = get(context, "functionName");
+    const records = get(event, "Records", []);
+    await Promise.all(records.map(async (record) => await processRecord(JSON.parse(get(record, "body", "")))));
 };
+
+async function processRecord(record) {
+    const reference = get(record, "[0].reference", v4());
+    await Promise.all(
+        dataArray.map(async (data, index) => {
+            console.info(`🙂 -> file: index.js:16 -> index:`, index);
+            console.info(`🙂 -> file: index.js:17 -> data:`, data);
+            const pKey = reference;
+            const sKey = index === 0 ? "Main" : get(data, "carrier", index);
+            const status = get(data, "status");
+            const payload = get(data, "payload");
+            const response = get(data, "response");
+            await insertIntoTransactionTable({ pKey, sKey, status, payload, response });
+        })
+    );
+}
+
+async function insertIntoTransactionTable({ pKey, sKey, status, payload, response }) {
+    const insertParams = {
+        TableName: LTL_LOG_TABLE,
+        Item: {
+            pKey,
+            sKey,
+            status,
+            payload,
+            response,
+            expiration: Math.floor(new Date(moment().add(7, "days").format()).getTime() / 1000),
+            lastUpdateId: functionName,
+            lastUpdateTime: moment.tz("America/Chicago").format(),
+        },
+    };
+    console.info(`🙂 -> file: index.js:29 -> insertParams:`, insertParams);
+    try {
+        return dynamoDB.put(insertParams).promise();
+    } catch (err) {
+        console.error(`🙂 -> file: index.js:32 -> err:`, err);
+        throw err;
+    }
+}
