fd14c7f0b9eceb75bf3d5b3056a2e785843a330d
jahir-biz <jahir.uddin@bizcloudexperts.com>
2023-11-23 18:23:29 +0530

    trigger added for lambda function

--- src/ltl_batch_rating/index.js
+++ src/ltl_batch_rating/index.js
@@ -1,8 +1,28 @@
-module.exports.handler = (event, context) => {
-    console.log(JSON.stringify(event, null, 2));
+let limit = 20;
+let offset = null;
+module.exports.handler = async (event, context, callback) => {
+    console.info(`🙂 -> file: index.js:4 -> event:`, event);
+    if (offset === null) {
+        return await startNextStep({ event, limit, offset });
+    }
     if (new Date().getTime() % 2 === 0) {
-        return { hasMoreData: "true" };
+        return callback(null, { hasMoreData: "true", limit, offset });
     } else {
-        return { hasMoreData: "false" };
+        return callback(null, { hasMoreData: "false", limit, offset });
     }
 };
+
+async function startNextStep(input) {
+    try {
+        const params = {
+            stateMachineArn: process.env.LTL_BATCH_RATING_STATE_MACHINE,
+            input: JSON.stringify({ input }),
+        };
+        const stepfunctions = new AWS.StepFunctions();
+        await stepfunctions.startExecution(params).promise();
+        return true;
+    } catch (error) {
+        console.error("Error in startNextStep:", error);
+        throw error;
+    }
+}
