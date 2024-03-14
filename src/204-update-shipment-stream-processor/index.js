95c0bad2d7e1468da9384afb7c099fc22d6883e6
Mohammed Sazeed <mohammed.sazeed@bizcloudexperts.com>
2024-03-15 00:25:57 +0530

    feat: 204 updates with update conditions

--- src/204-update-shipment-stream-processor/index.js
+++ src/204-update-shipment-stream-processor/index.js
@@ -1,7 +1,7 @@
 'use strict';
 const AWS = require('aws-sdk');
-const { get, isEqual } = require('lodash');
-const { updateOrders } = require('../shared/204-payload-generator/apis');
+const { get, isEqual, cloneDeep, isArray, includes } = require('lodash');
+const { updateOrders, getOrders } = require('../shared/204-payload-generator/apis');
 const { getCstTime, getTimezone } = require('../shared/204-payload-generator/helper');
 const { STATUSES } = require('../shared/constants/204_create_shipment');
 
@@ -35,65 +35,95 @@ module.exports.handler = async (event) => {
         }
       }
 
-      console.info('Changed fields:', changedFields);
-
-      if (
-        get(changedFields, 'PickupTimeRange') ||
-        get(changedFields, 'PickUpDateTime') ||
-        get(changedFields, 'DeliveryDateTime') ||
-        get(changedFields, 'DeliveryTimeRange')
-      ) {
-        const { shipperTimeZone, consigneeTimeZone } =
-          await getShipperAndConsigneeTimezones(orderNo);
-        if (changedFields.PickUpDateTime) {
-          changedFields.PickUpDateTime = await getCstTime({
-            datetime: get(changedFields, 'PickUpDateTime'),
-            timezone: shipperTimeZone,
-          });
-        }
-        if (changedFields.PickupTimeRange) {
-          changedFields.PickupTimeRange = await getCstTime({
-            datetime: get(changedFields, 'PickupTimeRange'),
-            timezone: shipperTimeZone,
-          });
-        }
-        if (changedFields.DeliveryDateTime) {
-          changedFields.DeliveryDateTime = await getCstTime({
-            datetime: get(changedFields, 'DeliveryDateTime'),
-            timezone: consigneeTimeZone,
-          });
-        }
-        if (changedFields.DeliveryTimeRange) {
-          changedFields.DeliveryTimeRange = await getCstTime({
-            datetime: get(changedFields, 'DeliveryTimeRange'),
-            timezone: consigneeTimeZone,
-          });
-        }
-      }
       console.info('🚀 ~ file: index.js:66 ~ event.Records.map ~ changedFields:', changedFields);
 
       // Query log table
       const logQueryResult = await queryOrderStatusTable(orderNo);
       if (logQueryResult.length > 0 && logQueryResult[0].Status === STATUSES.FAILED) {
         // retrigger the process by updating the status to 'PENDING'
-        await updateOrderStatusTable({ orderNo });
+        return await updateOrderStatusTable({ orderNo });
       } else if (logQueryResult.length > 0 && logQueryResult[0].Status === STATUSES.SENT) {
         const response = get(logQueryResult, '[0]Response');
-        console.info('🚀 ~ file: index.js:42 ~ event.Records.map ~ Response:', response);
-        const stopsFromResponse = get(response, 'stops', []);
+
+        const id = get(response, 'id', '');
+        console.info('🚀 ~ file: index.js:46 ~ event.Records.map ~ id:', id);
+        if (id === '') {
+          console.info('Skipping the process as the shipment id is not available');
+          return true;
+        }
+        const orderResponse = await getOrders({ id });
+        console.info('🚀 ~ file: index.js:43 ~ event.Records.map ~ orderResponse:', orderResponse);
+
+        const movements = get(orderResponse, 'movements');
+
+        // Check if movements is an array and if its length is greater than 1
+        if (isArray(movements) && movements.length > 1) {
+          console.info('There are more than 1 movement arrays in the object.');
+          return true;
+        }
+        console.info('There are only 1 movement arrays in the object.');
+        // check if the movements array has brokerage_status as  "OPEN", "BOOKED", "NEWOMNI", or "COVERED"
+        const brokerageStatus = get(movements, '[0].brokerage_status', '');
+        if (!includes(['OPEN', 'BOOKED', 'NEWOMNI', 'COVERED'], brokerageStatus)) {
+          console.info('Brokerage status is not OPEN, BOOKED, NEWOMNI, or COVERED.');
+          return true;
+        }
+        if (
+          get(changedFields, 'PickupTimeRange') ||
+          get(changedFields, 'PickupDateTime') ||
+          get(changedFields, 'DeliveryDateTime') ||
+          get(changedFields, 'DeliveryTimeRange')
+        ) {
+          const { shipperTimeZone, consigneeTimeZone } =
+            await getShipperAndConsigneeTimezones(orderNo);
+          if (changedFields.PickupDateTime) {
+            changedFields.PickupDateTime = await getCstTime({
+              datetime: get(changedFields, 'PickupDateTime'),
+              timezone: shipperTimeZone,
+            });
+          }
+          if (changedFields.PickupTimeRange) {
+            changedFields.PickupTimeRange = await getCstTime({
+              datetime: get(changedFields, 'PickupTimeRange'),
+              timezone: shipperTimeZone,
+            });
+          }
+          if (changedFields.DeliveryDateTime) {
+            changedFields.DeliveryDateTime = await getCstTime({
+              datetime: get(changedFields, 'DeliveryDateTime'),
+              timezone: consigneeTimeZone,
+            });
+          }
+          if (changedFields.DeliveryTimeRange) {
+            changedFields.DeliveryTimeRange = await getCstTime({
+              datetime: get(changedFields, 'DeliveryTimeRange'),
+              timezone: consigneeTimeZone,
+            });
+          }
+        }
+
+        const stopsFromResponse = cloneDeep(get(orderResponse, 'stops', []));
         console.info(
-          '🚀 ~ file: index.js:45 ~ event.Records.map ~ stopsFromResponse:',
+          '🚀 ~ file: index.js:81 ~ event.Records.map ~ stopsFromResponse:',
           stopsFromResponse
         );
-        const updatedStops = await updateStopsFromChangedFields(stopsFromResponse, changedFields);
-        console.info('🚀 ~ file: index.js:54 ~ event.Records.map ~ updatedStops:', updatedStops);
+        const updatedStops = await updateStopsFromChangedFields(
+          stopsFromResponse,
+          changedFields,
+          brokerageStatus
+        );
+        console.info('🚀 ~ file: index.js:82 ~ event.Records.map ~ updatedStops:', updatedStops);
+
         // In the response we need to update the stops array and need to send the entire response with the updates stops array
-        const updatedResponse = { ...response, stops: updatedStops };
-        if (isEqual(response, updatedResponse)) {
+        const updatedResponse = cloneDeep(orderResponse);
+        updatedResponse.stops = updatedStops;
+
+        if (isEqual(orderResponse, updatedResponse)) {
           console.info(`Payload is the same as the old payload. Skipping further processing.
-                        \n Payload: ${JSON.stringify(updatedResponse)}`);
-          return; // Skip further processing if the payload is the same as the old payload
+                \n Payload: ${JSON.stringify(updatedResponse)}`);
+          return 'Payload is the same as the old payload. Skipping further processing';
         }
+        console.info('Condition Satisfied. calling update orders api');
         // Send update
         await updateOrders({ payload: updatedResponse });
         // UPDATE updatecount in orderstatus table - new columns - UpdatePayload, UpdateCount
@@ -105,6 +135,7 @@ module.exports.handler = async (event) => {
       } else {
         console.info('Update not found in log table.');
       }
+      return true;
     })
   );
 };
@@ -168,7 +199,7 @@ async function setUpdateCount({ orderNo, UpdateCount, updatedResponse }) {
 }
 
 // Function to update stops based on changed fields
-async function updateStopsFromChangedFields(stops, changedFields) {
+async function updateStopsFromChangedFields(stops, changedFields, brokerageStatus) {
   for (const stop of stops) {
     // eslint-disable-next-line camelcase
     const { order_sequence } = stop;
@@ -176,11 +207,11 @@ async function updateStopsFromChangedFields(stops, changedFields) {
     // eslint-disable-next-line camelcase
     if (order_sequence === 1) {
       // Handle Pickup
-      updatePickupFields(stop, changedFields);
+      updatePickupFields(stop, changedFields, brokerageStatus);
       // eslint-disable-next-line camelcase
     } else if (order_sequence === 2) {
       // Handle Delivery
-      updateDeliveryFields(stop, changedFields);
+      updateDeliveryFields(stop, changedFields, brokerageStatus);
     }
   }
 
@@ -188,70 +219,78 @@ async function updateStopsFromChangedFields(stops, changedFields) {
 }
 
 // Function to update pickup fields
-function updatePickupFields(stop, changedFields) {
-  if (changedFields.ShipName) {
-    stop.location_name = changedFields.ShipName;
+function updatePickupFields(stop, changedFields, brokerageStatus) {
+  if (includes(['OPEN', 'BOOKED', 'NEWOMNI', 'COVERED'], brokerageStatus)) {
+    if (changedFields.PickupDateTime) {
+      stop.sched_arrive_early = changedFields.PickupDateTime;
+    }
+    if (changedFields.PickupTimeRange) {
+      stop.sched_arrive_late = changedFields.PickupTimeRange;
+    }
+    if (changedFields.ShipContact) {
+      stop.contact_name = changedFields.ShipContact;
+    }
+    if (changedFields.ShipPhone) {
+      stop.phone = changedFields.ShipPhone;
+    }
   }
-  if (changedFields.ShipContact) {
-    stop.contact_name = changedFields.ShipContact;
-  }
-  if (changedFields.ShipZip) {
-    stop.zip_code = changedFields.ShipZip;
-  }
-  if (changedFields.ShipCity) {
-    stop.city_name = changedFields.ShipCity;
-  }
-  if (changedFields.ShipPhone) {
-    stop.phone = changedFields.ShipPhone;
-  }
-  if (changedFields.ShipAddress1) {
-    stop.address = changedFields.ShipAddress1;
-  }
-  if (changedFields.ShipAddress2) {
-    stop.address2 = changedFields.ShipAddress2;
-  }
-  if (changedFields.FK_ShipState) {
-    stop.state = changedFields.FK_ShipState;
-  }
-  if (changedFields.PickUpDateTime) {
-    stop.sched_arrive_early = changedFields.PickUpDateTime;
-  }
-  if (changedFields.PickupTimeRange) {
-    stop.sched_arrive_late = changedFields.PickupTimeRange;
+  if (includes(['OPEN', 'NEWOMNI'], brokerageStatus)) {
+    if (changedFields.ShipName) {
+      stop.location_name = changedFields.ShipName;
+    }
+    if (changedFields.ShipZip) {
+      stop.zip_code = changedFields.ShipZip;
+    }
+    if (changedFields.ShipCity) {
+      stop.city_name = changedFields.ShipCity;
+    }
+    if (changedFields.ShipAddress1) {
+      stop.address = changedFields.ShipAddress1;
+    }
+    if (changedFields.ShipAddress2) {
+      stop.address2 = changedFields.ShipAddress2;
+    }
+    if (changedFields.FK_ShipState) {
+      stop.state = changedFields.FK_ShipState;
+    }
   }
 }
 
 // Function to update delivery fields
-function updateDeliveryFields(stop, changedFields) {
-  if (changedFields.ConAddress1) {
-    stop.address = changedFields.ConAddress1;
+function updateDeliveryFields(stop, changedFields, brokerageStatus) {
+  if (includes(['OPEN', 'BOOKED', 'NEWOMNI', 'COVERED'], brokerageStatus)) {
+    if (changedFields.ConContact) {
+      stop.contact_name = changedFields.ConContact;
+    }
+    if (changedFields.ConPhone) {
+      stop.phone = changedFields.ConPhone;
+    }
+    if (changedFields.DeliveryDateTime) {
+      stop.sched_arrive_early = changedFields.DeliveryDateTime;
+    }
+    if (changedFields.DeliveryTimeRange) {
+      stop.sched_arrive_late = changedFields.DeliveryTimeRange;
+    }
   }
-  if (changedFields.ConAddress2) {
-    stop.address2 = changedFields.ConAddress2;
-  }
-  if (changedFields.ConCity) {
-    stop.city_name = changedFields.ConCity;
-  }
-  if (changedFields.ConContact) {
-    stop.contact_name = changedFields.ConContact;
-  }
-  if (changedFields.ConName) {
-    stop.location_name = changedFields.ConName;
-  }
-  if (changedFields.ConPhone) {
-    stop.phone = changedFields.ConPhone;
-  }
-  if (changedFields.DeliveryDateTime) {
-    stop.sched_arrive_early = changedFields.DeliveryDateTime;
-  }
-  if (changedFields.DeliveryTimeRange) {
-    stop.sched_arrive_late = changedFields.DeliveryTimeRange;
-  }
-  if (changedFields.FK_ConState) {
-    stop.state = changedFields.FK_ConState;
-  }
-  if (changedFields.ConZip) {
-    stop.zip_code = changedFields.ConZip;
+  if (includes(['OPEN', 'NEWOMNI'], brokerageStatus)) {
+    if (changedFields.ConAddress1) {
+      stop.address = changedFields.ConAddress1;
+    }
+    if (changedFields.ConAddress2) {
+      stop.address2 = changedFields.ConAddress2;
+    }
+    if (changedFields.ConCity) {
+      stop.city_name = changedFields.ConCity;
+    }
+    if (changedFields.ConName) {
+      stop.location_name = changedFields.ConName;
+    }
+    if (changedFields.FK_ConState) {
+      stop.state = changedFields.FK_ConState;
+    }
+    if (changedFields.ConZip) {
+      stop.zip_code = changedFields.ConZip;
+    }
   }
 }
 
