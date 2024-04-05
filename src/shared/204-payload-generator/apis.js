1ce7041110545afb9f9c4e6b9f2da7a08e14984f
Mohammed Sazeed <mohammed.sazeed@bizcloudexperts.com>
2024-04-05 15:29:07 +0530

    feat: bugfixes

--- src/shared/204-payload-generator/apis.js
+++ src/shared/204-payload-generator/apis.js
@@ -12,10 +12,7 @@ const {
   TRACKING_NOTES_API_URL,
   API_PASS,
   API_USER_ID,
-  ADDRESS_MAPPING_G_API_KEY,
 } = process.env;
-
-const apiKey = ADDRESS_MAPPING_G_API_KEY;
 
 async function getLocationId(name, address1, address2, state) {
   const handleSpecialCharacters = (inputString) => {
@@ -60,33 +57,52 @@ async function getLocationId(name, address1, address2, state) {
     const queryParams = new URLSearchParams(combination);
     const apiUrl = `${apiUrlBase}?${queryParams}`;
 
-    console.info('🚀 ~ file: apis.js:62 ~ getLocationId ~ apiUrl:', apiUrl)
+    console.info('🚀 ~ file: apis.js:62 ~ getLocationId ~ apiUrl:', apiUrl);
     try {
       const response = await axios.get(apiUrl, { headers });
-      const responseData = _.get(response, 'data', {});
+      const responseData = _.get(response, 'data', []);
 
-      // Remove asterisks from modifiedName, modifiedAddress1, and modifiedAddress2
-      const cleanedName = modifiedName.replace('*', '');
-      const cleanedAddress1 = modifiedAddress1.replace('*', '');
-      const cleanedAddress2 = modifiedAddress2 ? modifiedAddress2.replace('*', '') : modifiedAddress2;
+      // Add a check to ensure responseData is not null before filtering
+      if (responseData !== null && responseData.length > 0) {
+        // Remove asterisks from modifiedName, modifiedAddress1, and modifiedAddress2
+        const cleanedName = modifiedName.replace('*', '');
+        const cleanedAddress1 = modifiedAddress1.replace('*', '');
+        const cleanedAddress2 = modifiedAddress2
+          ? modifiedAddress2.replace('*', '')
+          : modifiedAddress2;
 
-      // Filter response data to ensure all fields match the provided parameters
-      const filteredData = responseData.filter(
-        (item) =>
-          (_.toUpper(cleanedName) === _.toUpper(item.name) ||
-            _.startsWith(_.toUpper(item.name), _.toUpper(cleanedName))) &&
-          (_.toUpper(cleanedAddress1) === _.toUpper(item.address1) ||
-            _.startsWith(_.toUpper(item.address1), _.toUpper(cleanedAddress1))) &&
-          (_.isEmpty(cleanedAddress2) ||
-            _.toUpper(cleanedAddress2) === _.toUpper(item.address2) ||
-            _.startsWith(_.toUpper(item.address2), _.toUpper(cleanedAddress2))) &&
-          _.toUpper(item.state) === _.toUpper(state)
-      );
+        // Filter response data to ensure all fields match the provided parameters
+        const filteredData = responseData.filter(
+          (item) =>
+            (_.toUpper(removePunctuation(cleanedName)) ===
+              _.toUpper(removePunctuation(item.name)) ||
+              _.startsWith(
+                _.toUpper(removePunctuation(item.name)),
+                _.toUpper(removePunctuation(cleanedName))
+              )) &&
+            (_.toUpper(removePunctuation(cleanedAddress1)) ===
+              _.toUpper(removePunctuation(item.address1)) ||
+              _.startsWith(
+                _.toUpper(removePunctuation(item.address1)),
+                _.toUpper(removePunctuation(cleanedAddress1))
+              )) &&
+            (_.isEmpty(cleanedAddress2) ||
+              _.toUpper(removePunctuation(cleanedAddress2)) ===
+                _.toUpper(removePunctuation(item.address2)) ||
+              _.startsWith(
+                _.toUpper(removePunctuation(item.address2)),
+                _.toUpper(removePunctuation(cleanedAddress2))
+              )) &&
+            _.toUpper(removePunctuation(item.state)) === _.toUpper(removePunctuation(state))
+        );
 
-      if (!_.isEmpty(filteredData)) {
-        console.info('🙂 -> filteredData[0]:', filteredData[0]);
-        console.info('🙂 -> filteredData[0].id:', filteredData[0].id);
-        return _.get(filteredData, '[0].id', false);
+        if (!_.isEmpty(filteredData)) {
+          console.info('🙂 -> filteredData[0]:', filteredData[0]);
+          console.info('🙂 -> filteredData[0].id:', filteredData[0].id);
+          return _.get(filteredData, '[0].id', false);
+        }
+      } else {
+        console.error('🙂 -> file: apis.js:34 -> getLocationId -> responseData is null');
       }
     } catch (error) {
       console.error('🙂 -> file: apis.js:34 -> getLocationId -> error:', error);
@@ -94,6 +110,10 @@ async function getLocationId(name, address1, address2, state) {
     }
   }
   return false;
+}
+
+function removePunctuation(str) {
+  return str.replace(/[^\w\s]/g, '');
 }
 
 async function createLocation({ data, orderId, consolNo = 0, country, houseBill }) {
@@ -249,49 +269,10 @@ async function liveSendUpdate(houseBill, shipmentId) {
   }
 }
 
-async function checkAddressByGoogleApi(address) {
-  try {
-    const geocodeResponse = await axios.get(
-      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`
-    );
-
-    if (geocodeResponse.data.status !== 'OK') {
-      throw new Error(`Unable to geocode ${address}`);
-    }
-
-    const { lat, lng } = _.get(geocodeResponse, 'data.results[0].geometry.location', {});
-    return { lat, lng };
-  } catch (error) {
-    console.error(`Error geocoding address "${address}":`, error.message);
-    throw error;
-  }
-}
-
-async function getTimezoneByGoogleApi(lat, long) {
-  try {
-    const timestamp = Date.now() / 1000;
-    const timezoneResponse = await axios.get(
-      `https://maps.googleapis.com/maps/api/timezone/json?location=${lat}%2C${long}&timestamp=${timestamp}&key=${apiKey}`
-    );
-
-    if (timezoneResponse.data.status !== 'OK') {
-      throw new Error(`Unable to fetch timezone for coordinates (${lat}, ${long})`);
-    }
-
-    const timeZoneId = _.get(timezoneResponse, 'data.timeZoneId');
-    return timeZoneId;
-  } catch (error) {
-    console.error(`Error fetching timezone for coordinates (${lat}, ${long}):`, error.message);
-    throw error;
-  }
-}
-
 module.exports = {
   getLocationId,
   createLocation,
   sendPayload,
   updateOrders,
   liveSendUpdate,
-  checkAddressByGoogleApi,
-  getTimezoneByGoogleApi,
 };
