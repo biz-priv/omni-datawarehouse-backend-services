c2b15aff64ed566a15bbca4570e9ecffee6eda7d
naveen03252 <naveen.chitturi@bizcloudexperts.com>
2024-07-03 14:44:22 +0530

    cancle shipment update (nav)

--- src/204-void-shipment-stream-processor/index.js
+++ src/204-void-shipment-stream-processor/index.js
@@ -1,11 +1,13 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+
+const sns = new AWS.SNS();
 const _ = require('lodash');
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 const { TYPES, VENDOR, STATUSES } = require('../shared/constants/204_create_shipment');
-const { getUserEmail } = require('../shared/204-payload-generator/helper');
+const { getUserEmail, sendSESEmail } = require('../shared/204-payload-generator/helper');
 const { updateOrders } = require('../shared/204-payload-generator/apis');
 
 const {
@@ -14,9 +16,15 @@ const {
   SHIPMENT_APAR_TABLE,
   SHIPMENT_HEADER_TABLE,
   SHIPMENT_APAR_INDEX_KEY_NAME,
+  APAR_FAILURE_TABLE,
+  LIVE_SNS_TOPIC_ARN,
+  STAGE,
 } = process.env;
 
-module.exports.handler = async (event) => {
+let functionName;
+module.exports.handler = async (event, context) => {
+  functionName = _.get(context, 'functionName');
+  console.info('🙂 -> file: index.js:8 -> functionName:', functionName);
   try {
     console.info('Event:', event);
 
@@ -28,6 +36,7 @@ module.exports.handler = async (event) => {
 };
 
 async function processRecord(record) {
+  let orderNo;
   try {
     const body = JSON.parse(_.get(record, 'body', ''));
     const message = JSON.parse(_.get(body, 'Message', ''));
@@ -53,17 +62,28 @@ async function processRecord(record) {
       );
 
       if (fkVendorIdUpdated || fkVendorIdDeleted) {
-        const orderNo = _.get(message, 'Keys.FK_OrderNo.S');
+        orderNo = _.get(message, 'Keys.FK_OrderNo.S');
         console.info('🚀 ~ file: index.js:52 ~ processRecord ~ orderNo:', orderNo);
-        const Body = Object.keys(newImage).length ? newImage : oldImage;
-        await processShipmentAparData({ orderId: orderNo, newImage: Body });
+        const aparFailureDataArray = await fetchAparFailureData(orderNo);
+        const highestObject = aparFailureDataArray.reduce((max, current) => {
+          return (current.FK_SeqNo > max.FK_SeqNo) ? current : max;
+        }, aparFailureDataArray[0]);
+        const Note = _.get(highestObject, 'Note');
+        console.info('note: ', Note);
+        const shipmentAparData = await fetchShipmentAparData(orderNo);
+        const FkServiceId = _.get(shipmentAparData, '[0].FK_ServiceId');
+        console.info('FkServiceId: ', FkServiceId);
+        if (shipmentAparData.length > 0 && shipmentAparData) {
+          const Body = Object.keys(newImage).length ? newImage : oldImage;
+          await processShipmentAparData({ orderId: orderNo, newImage: Body, note: Note });
+        }
       }
     } else if (
       dynamoTableName === SHIPMENT_HEADER_TABLE &&
       _.get(newImage, 'FK_OrderStatusId') === 'CAN'
     ) {
       console.info('🚀 ~ file: index.js:45 ~ processRecord ~ dynamoTableName:', dynamoTableName);
-      const orderNo = newImage.PK_OrderNo;
+      orderNo = newImage.PK_OrderNo;
       console.info('🚀 ~ file: index.js:46 ~ processRecord ~ orderId:', orderNo);
       const shipmentAparDataArray = await fetchOrderNos(orderNo);
       console.info(
@@ -82,7 +102,7 @@ async function processRecord(record) {
             // Process data for each orderId
             await Promise.all(
               aparDataArray.map(async (aparData) => {
-                await processShipmentAparData({ orderId, newImage: aparData });
+                await processShipmentAparData({ orderId, newImage: aparData, note: 'Shipment got Cancelled' });
               })
             );
           })
@@ -96,6 +116,12 @@ async function processRecord(record) {
     }
   } catch (error) {
     console.error('Error processing record:', error);
+    await publishSNSTopic({
+      message: `${error.message}\n
+                function name: `,
+      stationCode: 'SUPPORT',
+      subject: `An error occured while voiding the process ~ FK_OrderNo: ${orderNo}`,
+    });
     throw error;
   }
 }
@@ -135,96 +161,129 @@ async function queryConsolStatusTable(consolNo) {
   }
 }
 
-async function processShipmentAparData({ orderId, newImage }) {
-  const consolNo = parseInt(_.get(newImage, 'ConsolNo', null), 10);
-  console.info('🚀 ~ file: index.js:117 ~ processShipmentAparData ~ consolNo:', consolNo);
-  const serviceLevelId = _.get(newImage, 'FK_ServiceId');
-  console.info(
-    '🚀 ~ file: index.js:119 ~ processShipmentAparData ~ serviceLevelId:',
-    serviceLevelId
-  );
-  const vendorId = _.get(newImage, 'FK_VendorId', '').toUpperCase();
-  console.info('🚀 ~ file: index.js:121 ~ processShipmentAparData ~ vendorId:', vendorId);
-  const controlStation = _.get(newImage, 'FK_ConsolStationId');
-  console.info(
-    '🚀 ~ file: index.js:123 ~ processShipmentAparData ~ controlStation:',
-    controlStation
-  );
-  const userId = _.get(newImage, 'UpdatedBy');
-  console.info('🚀 ~ file: index.js:125 ~ processShipmentAparData ~ userId:', userId);
-  const consolidation = _.get(newImage, 'Consolidation');
-  const seqNo = _.get(newImage, 'SeqNo');
-
-  let userEmail;
-  let stationCode;
-  let type;
-
-  if (consolNo === 0 && ['HS', 'TL'].includes(serviceLevelId)) {
-    userEmail = await getUserEmail({ userId });
-    stationCode = controlStation === '' ? 'SUPPORT' : controlStation;
-    type = TYPES.NON_CONSOLE;
-  } else if (consolNo > 0 && ['HS', 'TL'].includes(serviceLevelId)) {
-    userEmail = await getUserEmail({ userId });
-    stationCode = controlStation === '' ? 'SUPPORT' : controlStation;
-    type = TYPES.CONSOLE;
-  } else if (!isNaN(consolNo) && consolNo !== null && serviceLevelId === 'MT') {
-    userEmail = await getUserEmail({ userId });
-    console.info('🚀 ~ file: index.js:105 ~ processShipmentAparData ~ userEmail:', userEmail);
-    stationCode = controlStation === '' ? 'SUPPORT' : controlStation;
-    console.info('🚀 ~ file: index.js:107 ~ processShipmentAparData ~ stationCode:', stationCode);
-    type = TYPES.MULTI_STOP;
-  }
-
-  if (type === TYPES.NON_CONSOLE) {
-    const orderStatusResult = await queryOrderStatusTable(orderId);
-    if (orderStatusResult.length > 0 && orderStatusResult[0].Status === STATUSES.SENT) {
-      const { id, stops } = _.get(orderStatusResult, '[0].Response', {});
-      const orderData = { __type: 'orders', company_id: 'TMS', id, status: 'V', stops };
-      await updateOrders({ payload: orderData, orderId, consolNo });
-      await updateStatusTables({ orderId, type });
-      console.info('Voided the WT orderId:', orderId, '#PRO:', id);
-    }
-  }
-  if (type === TYPES.CONSOLE) {
-    const orderStatusResult = await queryOrderStatusTable(consolNo);
-    const fetchedOrderData = await fetchOrderData({ consolNo });
-
-    if (
-      (orderStatusResult.length > 0 && orderStatusResult[0].Status === STATUSES.SENT) ||
-      (orderStatusResult.length > 0 &&
-        orderStatusResult[0].Status === STATUSES.SENT &&
-        fetchedOrderData.length === 0)
-    ) {
-      const { id, stops } = _.get(orderStatusResult, '[0].Response', {});
-      const orderData = { __type: 'orders', company_id: 'TMS', id, status: 'V', stops };
-      await updateOrders({ payload: orderData, orderId, consolNo });
-      await updateStatusTables({ orderId: consolNo, type });
-      console.info('Voided the WT orderId:', orderId, '#PRO:', id);
-    }
-  }
-  if (type === TYPES.MULTI_STOP) {
-    const consolStatusResult = await queryConsolStatusTable(consolNo);
+async function processShipmentAparData({ orderId, newImage, note }) {
+  try {
+    const consolNo = parseInt(_.get(newImage, 'ConsolNo', null), 10);
+    console.info('🚀 ~ file: index.js:117 ~ processShipmentAparData ~ consolNo:', consolNo);
+    const serviceLevelId = _.get(newImage, 'FK_ServiceId');
     console.info(
-      '🚀 ~ file: index.js:183 ~ processShipmentAparData ~ consolStatusResult:',
-      consolStatusResult
+      '🚀 ~ file: index.js:119 ~ processShipmentAparData ~ serviceLevelId:',
+      serviceLevelId
     );
-    const fetchedOrderData = await fetchOrderData({ consolNo });
+    const vendorId = _.get(newImage, 'FK_VendorId', '').toUpperCase();
+    console.info('🚀 ~ file: index.js:121 ~ processShipmentAparData ~ vendorId:', vendorId);
+    const controlStation = _.get(newImage, 'FK_ConsolStationId');
+    console.info(
+      '🚀 ~ file: index.js:123 ~ processShipmentAparData ~ controlStation:',
+      controlStation
+    );
+    const userId = _.get(newImage, 'UpdatedBy');
+    console.info('🚀 ~ file: index.js:125 ~ processShipmentAparData ~ userId:', userId);
+    const consolidation = _.get(newImage, 'Consolidation');
+    const seqNo = _.get(newImage, 'SeqNo');
 
-    if (
-      (consolStatusResult.length > 0 &&
-        consolStatusResult[0].Status === STATUSES.SENT &&
-        seqNo === '9999' &&
-        consolidation === 'Y') ||
-      (consolStatusResult.length > 0 &&
-        consolStatusResult[0].Status === STATUSES.SENT &&
-        fetchedOrderData.length === 0)
-    ) {
-      const { id, stops } = _.get(consolStatusResult, '[0].Response', {});
-      const orderData = { __type: 'orders', company_id: 'TMS', id, status: 'V', stops };
-      await updateOrders({ payload: orderData, orderId, consolNo });
-      await updateStatusTables({ consolNo, type });
-      console.info('Voided the WT consolNo:', consolNo, '#PRO:', id);
+    let userEmail;
+    let stationCode;
+    let type;
+
+    if (consolNo === 0 && ['HS', 'TL'].includes(serviceLevelId)) {
+      userEmail = await getUserEmail({ userId });
+      const fetchedStation = await getControllingStation(orderId);
+      if (fetchedStation !== '') {
+        stationCode = fetchedStation;
+      } else if (controlStation !== '') {
+        stationCode = controlStation;
+      } else {
+        stationCode = 'SUPPORT';
+      }
+      type = TYPES.NON_CONSOLE;
+    } else if (consolNo > 0 && ['HS', 'TL'].includes(serviceLevelId)) {
+      userEmail = await getUserEmail({ userId });
+      stationCode = controlStation === '' ? 'SUPPORT' : controlStation;
+      type = TYPES.CONSOLE;
+    } else if (!isNaN(consolNo) && consolNo !== null && serviceLevelId === 'MT') {
+      userEmail = await getUserEmail({ userId });
+      console.info('🚀 ~ file: index.js:105 ~ processShipmentAparData ~ userEmail:', userEmail);
+      stationCode = controlStation === '' ? 'SUPPORT' : controlStation;
+      console.info('🚀 ~ file: index.js:107 ~ processShipmentAparData ~ stationCode:', stationCode);
+      type = TYPES.MULTI_STOP;
     }
+
+    if (type === TYPES.NON_CONSOLE) {
+      const orderStatusResult = await queryOrderStatusTable(orderId);
+      if (orderStatusResult.length > 0 && orderStatusResult[0].Status === STATUSES.SENT) {
+        const { id, stops } = _.get(orderStatusResult, '[0].Response', {});
+        const orderData = { __type: 'orders', company_id: 'TMS', id, status: 'V', stops };
+        await updateOrders({ payload: orderData, orderId, consolNo });
+        await updateStatusTables({ orderId, type });
+        console.info('Voided the WT orderId:', orderId, '#PRO:', id);
+      }
+    }
+    if (type === TYPES.CONSOLE) {
+      const orderStatusResult = await queryOrderStatusTable(consolNo);
+      const fetchedOrderData = await fetchOrderData({ consolNo });
+
+      if (
+        (orderStatusResult.length > 0 && orderStatusResult[0].Status === STATUSES.SENT) ||
+        (orderStatusResult.length > 0 &&
+          orderStatusResult[0].Status === STATUSES.SENT &&
+          fetchedOrderData.length === 0)
+      ) {
+        const { id, stops } = _.get(orderStatusResult, '[0].Response', {});
+        const orderData = { __type: 'orders', company_id: 'TMS', id, status: 'V', stops };
+        await updateOrders({ payload: orderData, orderId, consolNo });
+        await updateStatusTables({ orderId: consolNo, type });
+        console.info('Voided the WT orderId:', orderId, '#PRO:', id);
+      }
+    }
+    if (type === TYPES.MULTI_STOP) {
+      const consolStatusResult = await queryConsolStatusTable(consolNo);
+      console.info(
+        '🚀 ~ file: index.js:183 ~ processShipmentAparData ~ consolStatusResult:',
+        consolStatusResult
+      );
+      const fetchedOrderData = await fetchOrderData({ consolNo });
+
+      if (
+        (consolStatusResult.length > 0 &&
+          consolStatusResult[0].Status === STATUSES.SENT &&
+          seqNo === '9999' &&
+          consolidation === 'Y') ||
+        (consolStatusResult.length > 0 &&
+          consolStatusResult[0].Status === STATUSES.SENT &&
+          fetchedOrderData.length === 0)
+      ) {
+        const { id, stops } = _.get(consolStatusResult, '[0].Response', {});
+        const orderData = { __type: 'orders', company_id: 'TMS', id, status: 'V', stops };
+        await updateOrders({ payload: orderData, orderId, consolNo });
+        await updateStatusTables({ consolNo, type });
+        console.info('Voided the WT consolNo:', consolNo, '#PRO:', id);
+      }
+      await sendSESEmail({
+        functionName,
+        message: `The shipment associated with the following details has been cancelled:\n
+                  Order ID: ${orderId}\n
+                  Consolidation Number: ${consolNo}\n
+                  Note: ${note}`,
+        userEmail,
+        subject: {
+          Data: `PB VOID NOTIFICATION - ${STAGE} ~ FK_OrderNo: ${orderId} ~ ConsolNo: ${consolNo}`,
+          Charset: 'UTF-8',
+        },
+      });
+      await publishSNSTopic({
+        message: `The shipment associated with the following details has been cancelled:\n
+                  Order ID: ${orderId}\n
+                  Consolidation Number: ${consolNo}\n
+                  Note: ${note}`,
+        stationCode,
+        subject: `PB VOID NOTIFICATION - ${STAGE} ~ FK_OrderNo: ${orderId} ~ ConsolNo: ${consolNo}`,
+      });
+    }
+  }
+  catch (error) {
+    console.error('Error in processShipmentAparData:', error);
+    throw error;
   }
 }
 
@@ -239,6 +298,60 @@ async function fetchOrderNos(orderId) {
     };
     const data = await dynamoDb.query(params).promise();
     return _.get(data, 'Items', []);
+  } catch (err) {
+    console.error('Error fetching order data:', err);
+    throw err;
+  }
+}
+
+async function fetchAparFailureData(orderId) {
+  try {
+    const params = {
+      TableName: APAR_FAILURE_TABLE,
+      KeyConditionExpression: 'FK_OrderNo = :orderNo',
+      ExpressionAttributeValues: {
+        ':orderNo': orderId,
+      },
+    };
+    const data = await dynamoDb.query(params).promise();
+    return _.get(data, 'Items', []);
+  } catch (err) {
+    console.error('Error fetching order data:', err);
+    throw err;
+  }
+}
+
+async function fetchShipmentAparData(orderId) {
+  try {
+    const params = {
+      TableName: SHIPMENT_APAR_TABLE,
+      KeyConditionExpression: 'FK_OrderNo = :orderNo',
+      FilterExpression:
+        'FK_ServiceId = :FK_ServiceId',
+      ExpressionAttributeValues: {
+        ':orderNo': orderId,
+        ':FK_ServiceId': 'SE'
+      },
+    };
+    const data = await dynamoDb.query(params).promise();
+    return _.get(data, 'Items', []);
+  } catch (err) {
+    console.error('Error shipment apar data:', err);
+    throw err;
+  }
+}
+
+async function getControllingStation(orderId) {
+  try {
+    const params = {
+      TableName: SHIPMENT_HEADER_TABLE,
+      KeyConditionExpression: 'PK_OrderNo = :orderNo',
+      ExpressionAttributeValues: {
+        ':orderNo': orderId,
+      },
+    };
+    const data = await dynamoDb.query(params).promise();
+    return _.get(data, 'Items[0].ControllingStation', '');
   } catch (err) {
     console.error('Error fetching order data:', err);
     throw err;
@@ -306,6 +419,27 @@ async function updateStatusTables({ consolNo, type, orderId }) {
     return 'success';
   } catch (error) {
     console.error('Error updating status table:', error);
+    throw error;
+  }
+}
+
+async function publishSNSTopic({ message, stationCode, subject }) {
+  try {
+    const params = {
+      TopicArn: LIVE_SNS_TOPIC_ARN,
+      Subject: subject,
+      Message: `An error occurred in ${functionName}: \n${message}`,
+      MessageAttributes: {
+        stationCode: {
+          DataType: 'String',
+          StringValue: stationCode.toString(),
+        },
+      },
+    };
+
+    await sns.publish(params).promise();
+  } catch (error) {
+    console.error('Error publishing to SNS topic:', error);
     throw error;
   }
 }
